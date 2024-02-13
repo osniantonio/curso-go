2fd4ffc43e5c32165ee54c34e7ccbde382bd43b2
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-13 13:15:48 -0300

    Testando em batch

--- Por-topico/14-Testing/01/tax_test.go
+++ Por-topico/14-Testing/01/tax_test.go
@@ -12,3 +12,23 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("Expected %f but go %f", expected, result)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expected {
+			t.Errorf("Expected %f but go %f", item.expected, result)
+		}
+	}
+}
