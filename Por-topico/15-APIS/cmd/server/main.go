5e3d4327d09b42c89697735c6505628e75fd0b34
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-23 17:47:30 -0300

    Documentando criação do User

--- Por-topico/15-APIS/cmd/server/main.go
+++ Por-topico/15-APIS/cmd/server/main.go
@@ -14,8 +14,25 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
+
+	_ "github.com/osniantonio/goexpert/15-APIS/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// @title Go Expert API Example
+// @version 1.0
+// @description Product API with authentication
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Osni Antônio Küchler
+// @contact.url https://www.linkedin.com/in/osni-ant%C3%B4nio-k%C3%BCchler-5487b13b
+// @contact.email osni.antonio@gmail.com
+
+// @host localhost:8000
+// @BasePath /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -58,6 +75,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJwt)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
