fa5206cf320170c9565c1ba849c3fc97a81458e7
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-02-24 14:18:42 -0300

    Range With WaitGroup

--- Por-topico/16-MULTITHREADING/4-Channels/4-Range-with-Wait-Group/main.go
+++ Por-topico/16-MULTITHREADING/4-Channels/4-Range-with-Wait-Group/main.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	ch := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(10)
 	go publish(ch)
-	reader(ch)
+	go reader(ch, &wg)
+	wg.Wait()
 }
 
-func reader(ch chan int) {
+func reader(ch chan int, wg *sync.WaitGroup) {
 	for x := range ch {
 		fmt.Printf("Received %d\n", x)
+		wg.Done()
 	}
 }
 
