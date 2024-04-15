5fbd541c3549929d3177bd400235b5c454cfa2e8
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-04-15 07:26:25 -0300

    Trabalhando com stream

--- Por-topico/20-GRPC/internal/service/category.go
+++ Por-topico/20-GRPC/internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/osniantonio/goexpert/20-gRPC/internal/database"
 	"github.com/osniantonio/goexpert/20-gRPC/internal/pb"
@@ -62,4 +63,29 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 		Description: category.Description,
 	}
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
