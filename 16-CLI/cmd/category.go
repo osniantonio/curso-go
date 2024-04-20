4d840dd2f305ba8f3c18d23cd6d45e785aa9ef33
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-04-20 14:27:27 -0300

    Inversão de controle ao executar comandos

--- 16-CLI/cmd/category.go
+++ 16-CLI/cmd/category.go
@@ -4,8 +4,6 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -20,20 +18,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("category called with name: " + category)
-		exists, _ := cmd.Flags().GetBool("exists")
-		fmt.Println("category called with exists: " + fmt.Sprint(exists))
-		id, _ := cmd.Flags().GetInt16("id")
-		fmt.Println("category called with id: " + fmt.Sprint(id))
-	},
-	PreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Chamado antes do Run")
-	},
-	PostRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Chamado depois do Run")
-	},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return fmt.Errorf("ocorreu um erro")
+		cmd.Help()
 	},
 }
 
@@ -41,7 +26,4 @@ var category string
 
 func init() {
 	rootCmd.AddCommand(categoryCmd)
-	categoryCmd.PersistentFlags().StringVarP(&category, "name", "n", "", "Category name")
-
-	categoryCmd.PersistentFlags().BoolP("exists", "e", false, "Check if category exists")
 }
